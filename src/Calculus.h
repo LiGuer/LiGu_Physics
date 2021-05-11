256d3bca0754eff3ff9a1a5d3f12736ee48b49ea
LiGuer <2975395623@qq.com>
2021-05-11 12:10:51 +0800

    Update

--- src/Calculus.h
+++ src/Calculus.h
@@ -18,7 +18,7 @@ double Cos		(double x, int N = 18);
 double lnOneAdd	(double x, int N = 18);
 double Arctan	(double x, int N = 18);
 double PowOneAdd(double x, double p, int N = 18);
-void   RungeKutta(Mat<>& y, double dt, double t0, int enpoch, Mat<>& (*derivY)(double t, Mat<>& y));
+void   RungeKutta(Mat<>& y, double dx, double x0, Mat<>& (*derivY)(double x, Mat<>& y), int enpoch = 1);
 																					\\解常微分方程组: RungeKutta法
 	   PoissonEquation();															\\Poisson's方程
 	   WaveEquation();																\\波动方程
@@ -83,6 +83,29 @@ double PartiDeriv2(Mat<>& x, int index, double dx, double(*f)(Mat<>& x)) {
 	return (t1 - t2 - t3 + t4) / (2 * dx * dx);
 }
 /******************************************************************************
+*                    Laplace 算子
+*	[定义]: ▽² ≡ ∂²/∂x² + ∂²/∂y² + ∂²/∂z² + ...
+*	[算法]: 有限差分法
+		[u(x+1,...) - 2·u(x,...) + u(x-1,...)] / Δx² + ...
+******************************************************************************/
+inline double LaplaceOperator(Mat<>& x, Mat<>& dx, double(*f)(Mat<>& x)) {
+	double ans = 0;
+	for (int dim = 0; dim < dx.size(); dim++) {
+		double t = 0;
+		x[dim] += 1;	t += f(x) / (dx[dim] * dx[dim]);		x[dim] -= 1;
+						t -= f(x) / (dx[dim] * dx[dim]) * 2;
+		x[dim] -= 1;	t += f(x) / (dx[dim] * dx[dim]);		x[dim] += 1;
+		ans += t / (dx[dim] * dx[dim]);
+	}
+	return ans;
+}
+inline double LaplaceOperator(Mat<>& u, int index, Mat<>& dx, int(*position)(int index, int dim, int delta)) {
+	double ans = 0;
+	for (int dim = 0; dim < dx.size(); dim++)
+		ans += (u[position(index, dim, 1)] - 2 * u[index] + u[position(index, dim, -1)]) / (dx[dim] * dx[dim]);
+	return ans;
+}
+/******************************************************************************
 *                    Taylor 展开
 *	[定义]: 
 		f(x) = f(x0)/0! + f'(x0)/1!·(x-x0) + ... + f^(n)(x0)/n!·(x-x0)
@@ -101,8 +124,8 @@ Mat<>& TaylorFormula(double x0, double(*f)(double x), Mat<>& Coeff, int N = 3) {
 		exp(x) = 1 + x + x^2/2! + x^3/3! + ...
 		sin(x) = x - x^3/3! + x^5/5! - x^7/7! + ...
 		cos(x) = 1 - x^2/2! + x^4/4! - x^6/6! + ...
-		ln(1+x)= x - x^2/2 + x^3/3 - x^4/4 + ...	x∈[-1,1]
-	  arctan(x)= x - x^3/3 + x^5/5 - x^7/7 + ...	x∈[-1,1]
+		ln(1+x)= x - x^2/2  + x^3/3  - x^4/4  + ...	x∈[-1,1]
+	  arctan(x)= x - x^3/3  + x^5/5  - x^7/7  + ...	x∈[-1,1]
 		(1+x)^p= 1 + px + p(p-1)/2!·x^2 + p(p-1)(p-2)/3!·x^2 + ...	|x| < 1
 ******************************************************************************/
 double Exp(double x, int N = 18) {
@@ -161,32 +184,33 @@ double integral(double xSt, double xEd, double(*f)(double x), int n) {
 /******************************************************************************
 *                    解常微分方程组: Runge Kutta 方法
 *	[公式]:           ->   ->       ->      ->
-		对于初值问题: y' = f(t, y)	y(t0) = y0
-		y[n+1] = y[n] + h/6·(k1 + 2·k2 + 2·k3 + k4)
-		k1 = f(tn , yn)					//区间开始斜率
-		k2 = f(tn + h/2 , yn + h/2·k1)	//区间中点斜率,通过欧拉法采用k1决定y在tn+h/2值
-		k3 = f(tn + h/2 , yn + h/2·k2)	//区间中点斜率,采用k2决定y值
-		k4 = f(tn + h , yn + h·k3)		//区间终点斜率
+		常微分方程组: y' = f(x, y)	y(x0) = y0
+		迭代求解 \vec y(x) 的一点/一区间的数值解.
+		y(x + dx) = y(x) + dx/6·(k1 + 2·k2 + 2·k3 + k4)
+		k1 = f(xn , yn)						//区间开始斜率
+		k2 = f(xn + dx/2, yn + dx/2·k1)	//区间中点斜率,通过欧拉法采用k1决定y在xn+dx/2值
+		k3 = f(xn + dx/2, yn + dx/2·k2)	//区间中点斜率,采用k2决定y值
+		k4 = f(xn + dx	, yn + dx  ·k3)	//区间终点斜率
 *	[目的]: 解常微分方程组
-		[ y1'(t) = f1(y1 , ... , yn , t)          ->   ->
-		| y2'(t) = f2(y1 , ... , yn , t)    =>    y' = f(t , y)
+		[ y1'(x) = f1(y1 , ... , yn , x)          ->   ->
+		| y2'(x) = f2(y1 , ... , yn , x)    =>    y' = f(x , y)
 		| ...
-		[ yn'(t) = fn(y1 , ... , yn , t)
+		[ yn'(x) = fn(y1 , ... , yn , x)
 *	[性质]:
 		* RK4法是四阶方法，每步误差是h⁵阶，总积累误差为h⁴阶
 ******************************************************************************/
-void RungeKutta(Mat<>& y, double dt, double t0, int enpoch, Mat<>& (*derivY)(double t, Mat<>& y)) {
+void RungeKutta(Mat<>& y, double dx, double x0, Mat<>& (*derivY)(double x, Mat<>& y), int enpoch = 1) {
 	Mat<> tmp, k, k1, k2, k3, k4;
-	double t = t0;
+	double x = x0;
 	while (enpoch--) {
 		// k1, k2, k3 ,k4
-		k1 = derivY(t, y);
-		k2 = derivY(t + dt / 2, tmp.add(y, tmp.mult(dt / 2, k1)));
-		k3 = derivY(t + dt / 2, tmp.add(y, tmp.mult(dt / 2, k2)));
-		k4 = derivY(t + dt,     tmp.add(y, tmp.mult(dt,		k3)));
+		k1 = derivY(x, y);
+		k2 = derivY(x + dx / 2, tmp.add(y, tmp.mult(dx / 2, k1)));
+		k3 = derivY(x + dx / 2, tmp.add(y, tmp.mult(dx / 2, k2)));
+		k4 = derivY(x + dx,     tmp.add(y, tmp.mult(dx,		k3)));
 		// y[n+1] = y[n] + h/6·(k1 + 2·k2 + 2·k3 + k4)
 		k.add(k.add(k1, k4), tmp.mult(2, tmp.add(k2, k3)));
-		y.add(y, k.mult(dt / 6, k));
+		y.add(y, k.mult(dx / 6, k));
 	};
 }
 /******************************************************************************
@@ -200,10 +224,9 @@ void RungeKutta(Mat<>& y, double dt, double t0, int enpoch, Mat<>& (*derivY)(dou
 		对于各种边界条件，Poisson's方程可能有许多种解，但每个解的梯度相同.
 		静电场情况下, 意味着在边界条件下的满足Poisson's方程的势函数，所解得的电场唯一确定.
 ******************************************************************************/
-Tensor<double>* PoissonEquation(Mat<>st, Mat<>ed, Mat<> delta, double (*f) (Mat<>& x)) {
+Tensor<double>* PoissonEquation(Mat<>& st, Mat<>& ed, Mat<>& delta, double(*f) (Mat<>& x)) {
 	// init
-	Mat<> tmp;
-	tmp.sub(ed, st);
+	Mat<> tmp; tmp.sub(ed, st);
 	Mat<int> dim; dim.E(st.rows);
 	for (int i = 0; i < st.rows; i++) dim[i] = (int)(tmp[i] / delta[i]);
 	Tensor<double>* Map = new Tensor<double>(dim.rows, dim.data);
@@ -232,19 +255,18 @@ Tensor<double>* PoissonEquation(Mat<>st, Mat<>ed, Mat<> delta, double (*f) (Mat<
 /******************************************************************************
 *                    波动方程
 *	[定义]: a ▽²u = ∂²u/∂t²
-			// ∂²u/∂t², 即.加速度
-			其中 ▽² ≡ ∂²/∂x² + ∂²/∂y² + ∂²/∂z² + ...
+			Laplace 算子 ▽² ≡ ∂²/∂x² + ∂²/∂y² + ∂²/∂z² + ...
 		边界条件:
-			[1] u(r,t = 0) = f(r)		//初位置
+			[1] u (r,t = 0) = f(r)		//初位置
 			[2] ∂u(r,t = 0)/∂t = g(r)	//初速度
 			[3]	u(a,t) = b				//边界值
-	[算法]: 有限差分法
+*	[算法]: 有限差分法
 		u(t+1,...) = 2·u(t,...) - u(t-1,...)
 				+ Δt²·a{[u(x+1,...) - 2·u(x,...) + u(x-1,...)]/Δx² + ...}
-	[推导]:
+*	[推导]:
 		∂²u/∂t² = { [u(t+1,rt) - u(t,rt)]/Δt - [u(t,rt) - u(t-1,rt)]/Δt } / Δt
 				= [u(t+1,rt) - 2·u(t,rt) + u(t-1,rt)] / Δt²
-		▽²u同理, 代入波动方程,
+		▽²u同理. 代入波动方程,
 		a { [u(x+1,...) - 2·u(x,...) + u(x-1,...)] / Δx² + ...}
 		  = [u(t+1,...) - 2·u(t,...) + u(t-1,...)] / Δt²
 		所以, 下一时刻有
@@ -252,35 +274,34 @@ Tensor<double>* PoissonEquation(Mat<>st, Mat<>ed, Mat<> delta, double (*f) (Mat<
 				+ Δt²·a{[u(x+1,...) - 2·u(x,...) + u(x-1,...)]/Δx² + ...}
 		特别的, 对于t = 1 * Δt时刻, 有
 		u(r,t=Δt) = u(r,t=0) + ∂u(r,t=0)/∂t·Δt + a▽²u·Δt²
+*	[流程]:
+		对于t = 1 * Δt时刻, 有 u(r,t=Δt) = u(r,t=0) + ∂u(r,t=0)/∂t·Δt + a▽²u·Δt²
+		u(t+1,...) = 2·u(t,...) - u(t-1,...)+Δt²·a{ [u(x + 1,...) - 2·u(x,...) + u(x - 1,...)] / Δx² + ... }
 	[*] 暂时只二维
 ******************************************************************************/
-void WaveEquation(Mat<>& Map, Mat<>& veloc, void (*setBoundaryEquations) (Mat<>& x, int time),
-	double alpha, double deltaTime, double deltaX, double deltaY, int EndTimes) {
-	Mat<> MapNow (Map), 
-		  MapPrev(Map);
-	//对于t = 1 * Δt时刻, 有 u(r,t=Δt) = u(r,t=0) + ∂u(r,t=0)/∂t·Δt + a▽²u·Δt²
-	for (int x = 1; x < Map.rows - 1; x++) {
-		for (int y = 1; y < Map.cols - 1; y++) {
-			MapNow(x, y) += veloc(x, y) * deltaTime + alpha * deltaTime * deltaTime * (
-				(Map(x + 1, y) - 2 * Map(x, y) + Map(x - 1, y)) / (deltaX * deltaX)
-			  + (Map(x, y + 1) - 2 * Map(x, y) + Map(x, y - 1)) / (deltaY * deltaY)
-			);
-		}
-	}setBoundaryEquations(MapNow, 0);
-	for (int time = 1; time < EndTimes; time++) {
-		//u(t+1,...) = 2·u(t,...) - u(t-1,...)+Δt²·a{ [u(x + 1,...) - 2·u(x,...) + u(x - 1,...)] / Δx² + ... }
-		for (int x = 1; x < Map.rows - 1; x++) {
-			for (int y = 1; y < Map.cols - 1; y++) {
-				MapPrev(x, y) = 2 * MapNow(x, y) - MapPrev(x, y) + alpha * deltaTime * deltaTime * (
-					(MapNow(x + 1, y) - 2 * MapNow(x, y) + MapNow(x - 1, y)) / (deltaX * deltaX)
-				  + (MapNow(x, y + 1) - 2 * MapNow(x, y) + MapNow(x, y - 1)) / (deltaY * deltaY)
+void WaveEquation(Mat<>& u, Mat<>& dudt, void (*boundaryFunc) (Mat<>& u, int time),
+	double A, double dt, double dx, double dy, int tEd) {
+	Mat<> uPrev = u;
+	for (int t = 1; t < tEd; t++) {
+		for (int y = 1; y < u.cols - 1; y++) 
+			for (int x = 1; x < u.rows - 1; x++)
+				uPrev(x, y) += (t == 0 ? dudt(x, y) * dt : -2 * u(x, y))
+				+ A * dt * dt * (
+				  (u(x + 1, y) - 2 * u(x, y) + u(x - 1, y)) / (dx * dx)
+				+ (u(x, y + 1) - 2 * u(x, y) + u(x, y - 1)) / (dy * dy)
 				);
-			}
-		}
-		MapPrev.swap(MapNow);
-		setBoundaryEquations(MapNow, time);
+		boundaryFunc(u.swap(uPrev), t);
 	}
-	Map = MapNow;
+}
+void WaveEquation(Mat<>& u, Mat<>& dudt, void (*boundaryFunc) (Mat<>& u, int time),
+	double A, double dt, Mat<>& dx, int tEd, int(*position)(int index, int dim, int delta)) {
+	Mat<> uPrev = u;
+	for (int t = 1; t < tEd; t++) {
+		for (int i = 0; i < u.size(); i++)
+			uPrev[i] += (t == 0 ? dudt[i] * dt : -2 * u[i])
+				+ A * dt * dt * LaplaceOperator(u, i, dx, position);
+		boundaryFunc(u.swap(uPrev), t);
+	}
 }
 /******************************************************************************
 *                    扩散方程
